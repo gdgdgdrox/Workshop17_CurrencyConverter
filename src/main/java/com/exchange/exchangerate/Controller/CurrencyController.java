12231ef4cb48c2ca2b11e4d95c23907ba12d3877
guodfoo <gdfoo94@gmail.com>
2022-08-08 22:14:19 +0800

    Used PostConstruct to retrieve Currency right after App start, before hitting controllers

--- src/main/java/com/exchange/exchangerate/Controller/CurrencyController.java
+++ src/main/java/com/exchange/exchangerate/Controller/CurrencyController.java
@@ -2,13 +2,12 @@ package com.exchange.exchangerate.Controller;
 
 import java.math.BigDecimal;
 
+import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import com.exchange.exchangerate.Model.Currency;
 import com.exchange.exchangerate.Model.ExchangeRate;
 import com.exchange.exchangerate.Service.ExchangeService;
@@ -19,14 +18,17 @@ public class CurrencyController {
     @Autowired
     private ExchangeService service;
 
-    @Autowired
-    private RedisTemplate redisTemplate;
+    //Task 1 requires us to get currency right after application is started. 
+    //A method annotated with @PostConstruct is executed after application started
+    //and Beans (e.g. CurrencyController, ExchangeService) have been instantiated
+    @PostConstruct
+    public void getCurrency(){
+        service.getCurrency();
+    }
 
     @GetMapping
     public String returnIndexPage(Model model){
-        service.getCurrency();
         model.addAttribute("currency", Currency.currencyCodeAndValue);
-        redisTemplate.opsForValue().set("example1", "examplezz");
         return "index";
     }
 
